5679f9edb1d36f946922344beb6966d52471f852
mtsmith051 <mtsmith051@gmail.com>
2023-03-29 21:44:02 -0500

    Debugs save to local storage feature in javascript.

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,22 @@
 $(document).ready(function () {
   var saveButton = $(".saveBtn")
   var inputArr = JSON.parse(localStorage.getItem('savedText')) || []
+
+  console.log(inputArr)
   
 
   saveButton.on("click", saveEvent) 
 
-  function saveEvent(event) {
-    var inputBtn = event.target
-    
-    var saveHour = $(inputBtn).parent().prop('id').split("-")[1];
-    
-    var textInput = $(inputBtn).siblings('.description').val();
+  function saveEvent(event) {    
+    var saveHour = $(this).parent().prop('id').split("-")[1];
+
+    var textInput = $(this).siblings('.description').val();
     
     var textObject = {
       hour: saveHour, 
       text: textInput
     };
+
     
     inputArr = inputArr.filter(({ hour }) => hour !== saveHour )
     inputArr.push(textObject);
@@ -25,14 +26,14 @@ $(document).ready(function () {
   };
 
   //Assigns different color to identify past present and future time slots.
-  function blockColor(){
+  function blockColor() {
     let currentTime = dayjs().format('HH');
     $(".time-block").each(function(index) {
       if (index > (currentTime - 9))
         $(this).attr("class", "row time-block future");
       if (index < (currentTime - 9))
         $(this).attr("class", "row time-block past");
-      if (index == (currentTime - 9))
+      if (index === (currentTime - 9))
         $(this).attr("class", "row time-block present");
     });
   }; 
@@ -47,17 +48,18 @@ $(document).ready(function () {
   userInput();
 
 // Adds a functioning date and time to header to identify correct date/time 
-function setTimeHeader() {
-  var currentDate = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a');
-  $('#currentDay').text(currentDate);
-
-  function updateTime(){
-    currentDate = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a');
+  function setTimeHeader() {
+    var currentDate = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a');
     $('#currentDay').text(currentDate);
+
+    function updateTime() {
+      currentDate = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a');
+      $('#currentDay').text(currentDate);
+    };
+
+    setInterval(updateTime, 1000);
+
   };
-  
-  setInterval(updateTime, 1000);
-};
-setTimeHeader();
+  setTimeHeader();
 
 });
